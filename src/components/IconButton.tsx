51f219b740d9c7ad8d4abbc4287cccae90292228
sagar Sen <sagarsen.dev@gmail.com>
2024-09-06 09:35:42 +0530

    Added prettier configuration

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 export interface IconButtonProps {
-    icon: React.ReactNode;
-    text: string;
+ readonly icon: React.ReactNode;
+ readonly text: string;
 }
 
-export default function IconButtonComponent({icon, text}: IconButtonProps) {
-    return (
-        <div>
-            <button className={"flex items-center justify-center gap-7 w-72 bg-white shadow-xl px-7 py-5 rounded-full hover:bg-gray-200"}>
-                {icon}
-                <span className={"text-3xl"}>{text}</span>
-            </button>
-        </div>
-    )
+export default function IconButtonComponent({ icon, text }: IconButtonProps) {
+ return (
+  <div>
+   <button
+    className={
+     'flex items-center justify-center gap-7 w-72 bg-white shadow-xl px-7 py-5 rounded-full hover:bg-gray-200'
+    }
+   >
+    {icon}
+    <span className={'text-3xl'}>{text}</span>
+   </button>
+  </div>
+ );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,23 @@
-import Image from "next/image";
-import logoWithSideName from "@/assets/logo-with-side-name.png"
-import { CgProfile } from "react-icons/cg";
+import Image from 'next/image';
+import logoWithSideName from '@/assets/logo-with-side-name.png';
+import { CgProfile } from 'react-icons/cg';
 
 export default function NavBar() {
-    return (
-        <div className={"flex w-full justify-between items-center"}>
-            <Image src={logoWithSideName} alt={"quilog"} width={250} height={250} className={""}/>
-            <div className={"flex items-center gap-3"}>
-                <h1 className={" hidden sm:block text-2xl font-semibold"}>Login or signup</h1>
-                <CgProfile className={"h-10 w-10 mt-1"}/>
-            </div>
-        </div>
-    )
+ return (
+  <div className={'flex w-full justify-between items-center'}>
+   <Image
+    src={logoWithSideName}
+    alt={'quilog'}
+    width={250}
+    height={250}
+    className={''}
+   />
+   <div className={'flex items-center gap-3'}>
+    <h1 className={' hidden sm:block text-2xl font-semibold'}>
+     Login or SignUp
+    </h1>
+    <CgProfile className={'h-10 w-10 mt-1'} />
+   </div>
+  </div>
+ );
 }
